d398cfcbbf05aff0ad402e5bdaf4a10a94164430
Pieter Muller <mullerp@amazon.co.uk>
2015-05-24 23:46:35 +0100

    Add MovementNoiseModel.

---
+++ src/main/java/za/co/pietermuller/playground/destructo/MovementNoiseModel.java
@@ -0,0 +1,68 @@
+package za.co.pietermuller.playground.destructo;
+
+public class MovementNoiseModel {
+
+    private class Gaussian {
+        private double mean;
+        private double sigma;
+
+        Gaussian(double mean, double sigma) {
+            this.mean = mean;
+            this.sigma = sigma;
+        }
+
+        /**
+         * Calculates the value of the guassian given scaling value with zero mean and unity sigma.
+         *
+         * @param scaler
+         * @return
+         */
+        double scaleGaussian(double scaler) {
+            return sigma + scaler + mean;
+        }
+    }
+
+    private final Gaussian distanceNoise;
+    private final Gaussian rotationNoise;
+
+    /**
+     * Creates a new instance.
+     *
+     * Mean and standard deviations define Gaussian distributions modelling the expected
+     * distance and rotation noise in response to a unity movement (ie 1 meter distance or 1 radian rotation).
+     *
+     * @param distanceNoiseMean in meters
+     * @param distanceNoiseSigma in meters
+     * @param rotationNoiseMean in radians
+     * @param rotationNoiseSigma in radians
+     */
+    public MovementNoiseModel(double distanceNoiseMean,
+                              double distanceNoiseSigma,
+                              double rotationNoiseMean,
+                              double rotationNoiseSigma) {
+        this.distanceNoise = new Gaussian(distanceNoiseMean, distanceNoiseSigma);
+        this.rotationNoise = new Gaussian(rotationNoiseMean, rotationNoiseSigma);
+    }
+
+    /**
+     * Returns the distance noise modelled by this instance, in response to a gaussian random.
+     * The input Gaussian Random must have mean 0, sigma 1.
+     *
+     * @param gaussianRandom
+     * @return distance noise, in meters
+     */
+    public double scaleDistanceGuassian(double gaussianRandom) {
+        return distanceNoise.scaleGaussian(gaussianRandom);
+    }
+
+    /**
+     * Returns the rotation noise modelled by this instance, in response to a gaussian random.
+     * The input Gaussian Random must have mean 0, sigma 1.
+     *
+     * @param gaussianRandom
+     * @return rotation noise, in radians
+     */
+    public Rotation scaleRotationGaussian(double gaussianRandom) {
+        return Rotation.radians(rotationNoise.scaleGaussian(gaussianRandom));
+    }
+}
---
+++ src/test/java/za/co/pietermuller/playground/destructo/MovementNoiseModelTest.java
@@ -0,0 +1,46 @@
+package za.co.pietermuller.playground.destructo;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class MovementNoiseModelTest {
+
+    @Test
+    public void testScaleDistanceGuassian() throws Exception {
+        // given
+        double mean = 100;
+        double sigma = 50;
+
+        double scalingRandom = -0.5;
+
+        MovementNoiseModel movementNoiseModel = new MovementNoiseModel(100, 50, 0, 0);
+
+        // when
+        double noise = movementNoiseModel.scaleDistanceGuassian(scalingRandom);
+
+        // then
+        double expectation = scalingRandom * sigma + mean;
+        assertThat(noise, is(equalTo(expectation)));
+    }
+
+    @Test
+    public void testScaleRotationGaussian() throws Exception {
+        // given
+        double mean = 100;
+        double sigma = 50;
+
+        double scalingRandom = -0.5;
+
+        MovementNoiseModel movementNoiseModel = new MovementNoiseModel(0, 0, 100, 50);
+
+        // when
+        Rotation noise = movementNoiseModel.scaleRotationGaussian(scalingRandom);
+
+        // then
+        double expectation = scalingRandom * sigma + mean;
+        assertThat(noise.radians(), is(equalTo(expectation)));
+    }
+}
